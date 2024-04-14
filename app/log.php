b777be41d043b7ea12fded37ce8ef9eda9b18380
lyucean <lyucean@gmail.com>
2024-04-14 17:54:39 +0300

    - убрал демона, пусть один скрипт отрабатывает все - добавил дату релиза - переписал класс логов на более простой

--- app/log.php
+++ app/log.php
@@ -3,59 +3,22 @@
 use Monolog\Logger;
 use Logtail\Monolog\LogtailHandler;
 
-class CustomLogger
+class Logs
 {
-    private Logger $logger;
-    private mixed $log_file;
-    private bool $useLogtail;
-    private int $pid;
+    private static $instance = null; // Static field to store the class instance
 
-    public function __construct(int $pid)
+    private function __construct()
     {
-        // Определяем, используем ли мы Logtail или логирование в файл
-        $this->useLogtail = isset($_ENV['LOGTAIL_TOKEN']);
+        $logger = new Logger('BSB ' . $_ENV['ENVIRONMENT']); // Initialize Logger
+        $logger->pushHandler(new LogtailHandler($_ENV['LOGTAIL_TOKEN']));
+        self::$instance = $logger; // Store the Logger instance
+    }
 
-        $this->pid = $pid;
-
-        if ($this->useLogtail) {
-            $this->logger = new Logger($this->pid);
-            $this->logger->pushHandler(new LogtailHandler($_ENV['LOGTAIL_TOKEN']));
+    public static function getInstance(): Logger
+    {
+        if (self::$instance === null) {
+            new self(); // Create instance of this class if not already created
         }
-
-        $this->log_file = $_ENV['LOG_FILE'] ?? 'default.log'; // Устанавливаем значение по умолчанию для файла логов
-    }
-
-    public function error($message): void
-    {
-        $this->log('error', $message);
-    }
-
-    public function info($message): void
-    {
-        $this->log('info', $message);
-    }
-
-    public function notice($message): void
-    {
-        $this->log('notice', $message);
-    }
-
-    public function warning($message): void
-    {
-        $this->log('warning', $message);
-    }
-
-    private function log($level, $message): void
-    {
-//        if ($this->useLogtail) {
-            // Используем Monolog для отправки сообщений в Logtail
-            $this->logger->{$level}($message, [
-              'pid' => $this->pid
-            ]);
-//        } else {
-            // Или записываем логи в файл
-            $logMessage = date('Y-m-d H:i:s')."|$this->pid|"." [$level] $message".PHP_EOL;
-            error_log($logMessage, 3, $this->log_file);
-//        }
+        return self::$instance; // Return the Logger instance
     }
 }
